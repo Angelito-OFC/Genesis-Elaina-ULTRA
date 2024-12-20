f0b09dec560f156e6c918b69f3e83d9935e254ef
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-12-20 14:42:46 -0400

    Actualizar dl-ytmp4.js

--- plugins/dl-ytmp4.js
+++ plugins/dl-ytmp4.js
@@ -2,11 +2,13 @@ import fetch from 'node-fetch'
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
   if (!text) return conn.reply(m.chat, `Ingresa un link de youtube`, m, fake)
+    await m.react('🕓')
   
   try {
     let api = await (await fetch(`https://api.siputzx.my.id/api/d/ytmp4?url=${text}`)).json()
     let dl_url = api.data.dl
 
+    await m.react('✅')
     await conn.sendMessage(m.chat, { video: { url: dl_url }, caption: null }, { quoted: m })
   } catch (error) {
     console.error(error)
