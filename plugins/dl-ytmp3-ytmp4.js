75ed01f80c7228cf19d5e0d2f8e34575dd0ab66d
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-12-20 14:47:58 -0400

    Actualizar dl-ytmp3-ytmp4.js

--- plugins/dl-ytmp3-ytmp4.js
+++ plugins/dl-ytmp3-ytmp4.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-  if (!text) return conn.reply(m.chat, `🤍 Ingresa un link de youtube`, m)
+  if (!text) return conn.reply(m.chat, `🎄 Ingresa un link de youtube`, m)
   await m.react('🕓')
 
   try {
