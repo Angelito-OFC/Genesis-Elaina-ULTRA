544c13332117d8a58eed31e29dbf2a1338ea9226
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-12-20 14:47:41 -0400

    Actualizar plugins

---
+++ plugins/dl-ytmp3-ytmp4.js
@@ -0,0 +1,34 @@
+import fetch from 'node-fetch'
+
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+  if (!text) return conn.reply(m.chat, `🤍 Ingresa un link de youtube`, m)
+  await m.react('🕓')
+
+  try {
+    if (command === 'ytmp3' || command === 'yta' || command === 'fgmp3') {
+      // Descargar audio
+      let api = await (await fetch(`https://api.siputzx.my.id/api/d/ytmp3?url=${text}`)).json()
+      let dl_url = api.data.dl
+      let title = api.data.title
+
+      await m.react('✅')
+      await conn.sendMessage(m.chat, { audio: { url: dl_url }, fileName: `${title}.mp3`, mimetype: 'audio/mp4' }, { quoted: m })
+    } else if (command === 'ytmp4' || command === 'ytv' || command === 'fgmp4') {
+      // Descargar video
+      let api = await (await fetch(`https://api.siputzx.my.id/api/d/ytmp4?url=${text}`)).json()
+      let dl_url = api.data.dl
+
+      await m.react('✅')
+      await conn.sendMessage(m.chat, { video: { url: dl_url }, caption: null }, { quoted: m })
+    }
+  } catch (error) {
+    console.error(error)
+    await m.react('❌')
+  }
+}
+
+handler.help = ['ytmp3 *<url>*', 'ytmp4 *<url>*'];
+handler.tags = ['downloader'];
+handler.command = ['ytmp3', 'yta', 'fgmp3', 'ytmp4', 'ytv', 'fgmp4']
+
+export default handler
