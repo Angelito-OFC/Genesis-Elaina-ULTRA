0c04d7c203c5edb60a80297e0e292b01669b5b3a
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-12-20 14:24:09 -0400

    Actualizar dl-ytmp3.js

--- plugins/dl-ytmp3.js
+++ plugins/dl-ytmp3.js
@@ -9,7 +9,7 @@ try {
 let api = await (await fetch(`https://api.siputzx.my.id/api/d/ytmp3?url=${text}`)).json()
 let dl_url = api.data.dl
 
-conn.sendMessage(m.chat, { audio: { url: dl_url }, mimetype: "audio/mp4", ptt: true }, { quoted: m })
+conn.sendMessage(m.chat, { audio: { url: dl_url }, mimetype: "audio/mp3", ptt: true }, { quoted: m })
 } catch (error) {
 console.error(error)
 }}
