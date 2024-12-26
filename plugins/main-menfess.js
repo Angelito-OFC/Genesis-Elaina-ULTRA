5fa69cbb3bb5e909175e829573b704093d119d03
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-12-25 22:30:23 -0400

    Actualizar main-menfess.js

--- plugins/main-menfess.js
+++ plugins/main-menfess.js
@@ -1,44 +1,63 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-    conn.menfess = conn.menfess ? conn.menfess : {}
-    if (!text) throw `*Cara penggunaan :*\n\n${usedPrefix + command} nomor|nama pengirim|pesan\n\n*Note:* nama pengirim boleh nama samaran atau anonymous.\n\n*Contoh:* ${usedPrefix + command} ${m.sender.split`@`[0]}|Anonymous|Hai.`;
-    let [jid, name, pesan] = text.split('|');
-    if ((!jid || !name || !pesan)) throw `*Cara penggunaan :*\n\n${usedPrefix + command} nomor|nama pengirim|pesan\n\n*Note:* nama pengirim boleh nama samaran atau anonymous.\n\n*Contoh:* ${usedPrefix + command} ${m.sender.split`@`[0]}|Anonymous|Hai.`;
-    jid = jid.replace(/[^0-9]/g, '') + '@s.whatsapp.net';
-    let data = (await conn.onWhatsApp(jid))[0] || {};
-    if (!data.exists) throw 'Nomer tidak terdaftar di whatsapp.';
-    if (jid == m.sender) throw 'tidak bisa mengirim pesan menfess ke diri sendiri.'
-    let mf = Object.values(conn.menfess).find(mf => mf.status === true)
-    if (mf) return !0
+    conn.menfess = conn.menfess ? conn.menfess : {};
+    if (!text) throw m.reply(`*🤍 Ejemplo:*\n\n${usedPrefix + command} numero mensaje\n\n*🤍 Uso:* ${usedPrefix + command} ${m.sender.split`@`[0]} Hola.`);
+    
+    let split = text.trim().split(/ (.+)/); 
+    let jid = split[0]; 
+    let pesan = split[1]; 
+
+    if (!jid || !pesan) throw m.reply(`*🤍 Ejemplo:*\n\n${usedPrefix + command} numero mensaje\n\n*🤍 Uso:* ${usedPrefix + command} ${m.sender.split`@`[0]} Hola.`);
+    
+    jid = jid.replace(/[^0-9]/g, '') + '@s.whatsapp.net'; 
+    let data = (await conn.onWhatsApp(jid))[0] || {}; 
+    if (!data.exists) throw m.reply('🤍 El número no está registrado en WhatsApp.');
+    if (jid == m.sender) throw m.reply('🤍 No puedes mandarte un mensaje a ti mismo.');
+    
+    let mf = Object.values(conn.menfess).find(mf => mf.status === true);
+    if (mf) return !0;
+    
+    let id = Math.floor(1000 + Math.random() * 9000); 
+    let teks = `*Hola* @${data.jid.split("@")[0]}, *recibiste un mensaje de confesión.*\n*Para* responder\n*Ejemplo: .respuesta <id> <Mensaje>*\n\n*\`ID:\`* *${id}*\n*\`MENSAJE:\`* \n\n${pesan}`.trim();
+    
     try {
-    	let id = + new Date
-        let tek = `Hai @${data.jid.split('@')[0]}, kamu menerima pesan Menfess nih.\n\nDari: *${name}*\nPesan: \n${pesan}\n\nMau balas pesan ini kak? bisa kok kak. tinggal ketik pesan kakak lalu kirim, nanti saya sampaikan ke *${name}*.`.trim();
-        await conn.reply(data.jid, tek, fakes)
-        .then(() => {
-            m.reply('Berhasil mengirim pesan menfess.')
+        
+        let sentMessage = await conn.sendMessage(data.jid, {
+            text: teks,
+            contextInfo: {
+                mentionedJid: [data.jid],
+                externalAdReply: {
+                    title: 'C O N F E S A R - G E N E S I S',
+                    body: '¡responder! .respuesta (id) (Mensaje)',
+                    mediaType: 1,
+                    renderLargerThumbnail: true,
+                    thumbnailUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIyz1dMPkZuNleUyfXPMsltHwKKdVddTf4-A&usqp=CAU',
+                    sourceUrl: 'https://whatsapp.com/channel/0029VaJxgcB0bIdvuOwKTM2Y'
+                }
+            }
+        });
+        
+        
+        if (sentMessage) {
             conn.menfess[id] = {
                 id,
                 dari: m.sender,
-                nama: name,
                 penerima: data.jid,
                 pesan: pesan,
-                status: false
-            }
-            return !0
-        })
+                status: false 
+            };
+            return conn.reply(m.chat, '*🤍 Confesión enviada con éxito.*\n*IDENTIFICADOR:*' + ` *${id}*`, m, fake);
+        }
+        
     } catch (e) {
-        console.log(e)
-        m.reply('eror');
+        console.error(e);
+        m.reply('❌ Ocurrió un error al enviar la respuesta.');
     }
 }
-handler.tags = ['main']
-handler.help = ['menfess', 'menfes'].map(v => v + ' <nomor|nama pengirim|pesan>')
-handler.command = /^(menfess|menfes)$/i
-handler.private = true
 
-export default handler
+handler.tags = ['tools'];
+handler.help = ['confesar'].map(v => v + ' <número mensaje>');
+handler.command = /^(mfs|confesar|memfes|confes)$/i;
+handler.register = true;
+handler.private = true;
 
-/* Made By FokusDotId (Fokus ID)
- * https://github.com/FokusDotId
- * Ingin bikin fitur tapi tidak bisa coding?
- * hubungi: https://wa.me/6281320170984
-*/
+export default handler;
